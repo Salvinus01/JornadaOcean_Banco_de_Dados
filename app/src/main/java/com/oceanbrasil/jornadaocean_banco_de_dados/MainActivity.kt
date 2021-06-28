2f260dedd40e4b824ee8c990f049a223ade07cda
Salvinus01 <86074085+Salvinus01@users.noreply.github.com>
2021-06-28 10:41:43 -0300

    Initial commit

---
+++ app/src/main/java/com/oceanbrasil/jornadaocean_banco_de_dados/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oceanbrasil.jornadaocean_banco_de_dados
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
